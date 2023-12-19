8a84ee43ba423d2edc4b5ea5d3d201552883a6cc
beqirajd <dejvis.beqiraj@linksmt.it>
2023-12-19 15:57:54 +0100

    fix root project

--- src/test/java/com/example/AggregateObjectWrapper.java
+++ src/test/java/com/example/AggregateObjectWrapper.java
@@ -8,6 +8,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @SpringBootTest
 public class AggregateObjectWrapper {
